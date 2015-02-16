83dba5b01f66a83ad57c8c32f84d486ca172d48d
Finlay McAfee <s1220880@barletta.inf.ed.ac.uk>
2015-02-16 22:41:09 +0000

    All still in draw_boundaries.m TODO separate sections into functions

--- src/draw_boundaries.m
+++ src/draw_boundaries.m
@@ -19,22 +19,25 @@ for i = 1 : N
     crops{i} = crop;
     
     BW = im2bw(crop, graythresh(crop));
-    
-    [B,L,N,A] = bwboundaries(BW);
-    figure, imshow(BW); hold on;
-    colors=['b' 'g' 'r' 'c' 'm' 'y'];
-    for k=1:length(B)
-        boundary = B{k};
-        cidx = mod(k,length(colors))+1;
-        plot(boundary(:,2), boundary(:,1),...
-             colors(cidx),'LineWidth',2);
+    BW2 = imcomplement(BW);
+    BW3 = imfill(BW2, 'holes');
+    STATS = regionprops(BW2, 'all');
+            
+        
+    %[B,L,N,A] = bwboundaries(BW2);
+    %figure, imshow(BW2); hold on;
+    %colors=['b' 'g' 'r' 'c' 'm' 'y'];
+    %for k=1:length(B)
+    %    boundary = B{k};
+    %    cidx = mod(k,length(colors))+1;
+    %    plot(boundary(:,2), boundary(:,1),...
+    %         colors(cidx),'LineWidth',2);
         %randomize text position for better visibility
-        rndRow = ceil(length(boundary)/(mod(rand*k,7)+1));
-        col = boundary(rndRow,2); row = boundary(rndRow,1);
-        h = text(col+1, row-1, num2str(L(row,col)));
-        set(h,'Color',colors(cidx),...
-            'FontSize',14,'FontWeight','bold');
-    end
-    figure; spy(A);
+    %    rndRow = ceil(length(boundary)/(mod(rand*k,7)+1));
+    %    col = boundary(rndRow,2); row = boundary(rndRow,1);
+    %    h = text(col+1, row-1, num2str(L(row,col)));
+    %    set(h,'Color',colors(cidx),...
+    %        'FontSize',14,'FontWeight','bold');
+    %end
 end
 
